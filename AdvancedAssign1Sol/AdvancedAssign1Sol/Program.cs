c1ec9d19d398886225c9d9aa06a1bbfae090793e
somayayasser3 <106781608+somayayasser3@users.noreply.github.com>
2024-07-26 15:05:10 +0300

    Add files via upload

---
+++ AdvancedAssign1Sol/AdvancedAssign1Sol/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace AdvancedAssign1Sol
+{
+    class Program
+    {
+        public static void BubbleSort(int []array)
+        {
+            bool Flag ;
+            for(int i = 0; i < array.Length; i++)
+            {
+                Flag = false;
+                for(int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if(array[j] > array[j + 1])
+                    {
+                        int Temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = Temp;
+                        Flag = true;
+                    }
+                }
+                if (!Flag) break;
+            }
+        }
+
+        public static void PrintArr(int []array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                Console.Write($"{array[i]} ");
+            Console.WriteLine("\n");
+        }
+        static void Main(string[] args)
+        {
+            #region Q1- Bubble Sort
+            //// We can Optimize Bubble Sort by Check on if Array is Already Sorted in The Firt Time in First For Loop 
+            //// This Means That The Second Loop will Be Done Only One Time Then Break From All Loops  [N]
+            //// so The Time Complexity is O(N) for second Loop Iteration [N] , First Loop Done One Time [1]
+            //int[] Numbers = { 12, 14, 0, 3, 6, 8, 7, 9, -1, 14, 13 };
+            //Console.WriteLine("Before Sorting: ");
+            //PrintArr(Numbers);
+            //BubbleSort(Numbers);
+            //Console.WriteLine("After Sorting: ");
+            //PrintArr(Numbers);
+            #endregion
+
+            #region Q2- Generic Range 
+            var R1 = new Range<int>(5, 12);
+            Console.WriteLine(R1.InRange(3));  
+            Console.WriteLine($"Length of range: {R1.Length()}"); 
+
+            var R2 = new Range<double>(5.3, 24.3);
+            Console.WriteLine(R2.InRange(12.5));  
+            Console.WriteLine($"Length of range: {R2.Length()}"); 
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdvancedAssign1Sol/AdvancedAssign1Sol/Range.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvancedAssign1Sol
+{
+    internal class Range<T> where T : IComparable
+    {
+        public T Minimum { get; set; }
+        public T Maximum { get; set; }
+
+        public Range(T minimum, T maximum)
+        {
+            if (minimum.CompareTo(maximum) >= 0) throw new Exception("Minimum Must be Less Than Maximum Value");
+
+            Minimum = minimum;
+            Maximum = maximum;
+        }
+
+        public bool InRange(T Value)
+        {
+            return (Minimum.CompareTo(Value) <= 0) && (Maximum.CompareTo(Value) >= 0);
+        }
+
+        public dynamic Length()
+        {
+            return ( (dynamic)Maximum - (dynamic)Minimum );
+            // Dynamic to Be Fit for int or double Data Type
+        }
+
+        public override string ToString()
+        {
+            return $"Min = {Minimum} , Maximum = {Maximum}";
+        }
+    }
+}
